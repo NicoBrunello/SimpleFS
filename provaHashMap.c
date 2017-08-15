d8458cf7ffa254513bbe0b1c94dcb1fa71f6d193
nicolobrunello <nicobrun96@gmail.com>
2017-08-15 12:21:20 +0200

    hash works

--- provaHashMap.c
+++ provaHashMap.c
@@ -6,59 +6,86 @@
 #define maxLen 255
 #define maxCom 10
 #define maxPath 65025
-#define N 588
+#define N 255
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+/*
 unsigned long hash(unsigned char *);
-int hashFunction (int ,int);
-void gen(char *);
+int hashFunction(int in  ,int j );
+void gen(char *);*/
 
 
-int main(int argc,char*argv[])
 
-	int hashmap[1024],i;
-	int collisioni=0;
-	char * str;
-	printf("ok");
-
-	for (i=0; i< 1500; i++){
-		gen(str); 
-		printf("%s \n", str);
-		
-	}
-	printf("%d\n", collisioni);
-	return 0;
-}
-
-int hashFunction (int in,int j){
-	
-	return i;
-}
-
-unsigned long hash(unsigned char *str)
-{
+unsigned long hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
     while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + c ; /*hash * 33 + c */
 
     return hash;
 }
 
-void gen(char *dst)
-{
+unsigned int hashFunction (unsigned char * str){
+	int j ;
+	j= hash(str)%1024;
+	
+	return j;
+}
+unsigned char* gen(unsigned char *dst) {
     int i, n;  
 
-    srand(time(NULL));               /* init seed */
-    if ((dst = malloc(N)) == NULL)   /* caller will need to free this */
-        return;
-    for (i = 0; i < N; )
-        if ((n = rand()) < 'A' && n > 'Z')
-            dst[i++] = n;
-    dst[N - 1] = 0;                   /* null terminate the string */
+     
+   	/* init seed */
+    if (dst = (char*)malloc(N*sizeof(char))) {  
+ 
+	   	while (i<N){
+
+	    	n =rand()%122;
+	    	if((n>65 && n<90) || (n>97 && n< 122)){
+				dst[i++] = n;
+	    	}
+	            
+	    }
+	    dst[N] = '\0';       
+	}
+	else{
+		printf("Allocaziona fallita");
+	}
+    /* null terminate the string */
+    return dst;
+    fre(dst);
 }
 
 
+int main(int argc,char*argv[]){
+	srand(time(NULL));
+	char* hashmap[1024][255];
+	int collisioni=0,index=0;
+	char * str;
+	for (int i= 0; i<1024; i++)
+		strcpy(hashmap[i], "--");
 
+	for (int i=0; i< 1024; i++){
+		str= gen(str); 
+		index = hashFunction(str);
+		printf("index --> %d\n", index);
+		while (hashmap[index]=="--"){
+			collisioni ++ ;
+			index ++;
+		}
+		strcpy(hashmap[index], str); ;
+		
+		
+	}
+	for(int i=0; i<=1024;i++){
+		printf(" %s | \n",hashmap[i]);
+
+	}
+	printf("\n%d\n", collisioni);
+	free (str);
+	return 0;
+}
--- simpleFS.c
+++ simpleFS.c
@@ -11,20 +11,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+
 
 //node structure
 typedef struct node_s{
 	char *name;
 	int numberOfSons;
 	int type; // 0= file , 1= directory
-	struct node_s *sons;
+	struct node_s *sons[1024];
 
 } node;
 
 
-void detectAction(char[]);
+node *  detectAction(char[], node * root);
 node* initTree(node*);
 unsigned long hash(unsigned char *);
+void getTokens(char[][maxLen+1], char[maxPath]);
+node* initNode(node * , char *);
+long hashFunction(unsigned char *, int);
 
 //da eliminare
 void scorriAlbero(node*);
@@ -40,29 +46,54 @@ int main(int argc,char*argv[]){
 	root= initTree(root);
 
 	while (!foundEOF){
-		scanf ("%s",command);
+		scanf ("%s",&command);
 		n= getchar();
 		if(n == EOF)
 			foundEOF= 1;
 		else
-			detectAction(command);
+			root=detectAction(command, root);
 	}
+	printf("Fine lettura\n");
 	scorriAlbero(root);
+	free(root);
 	return 0;
 }
 
-void detectAction(char input[maxCom]){
-	char path[maxPath];
+node* detectAction(char input[maxCom],node * root){
+	char path[maxPath], tokens[maxLen][maxLen+1];
+	int pathLen=0,i=0, index=0;
+	node * temp=root;
+
+	//initPath
+	for (int j=0; j<maxLen; j++){
+		strcpy(tokens[j], "--");
+	}
+
+	//detectAction
 	if(!strcmp(input,"create")){
 		scanf ("%s",&path);
+		getTokens(tokens, path );
+		while(strcmp(tokens[i], "--")){
+			i++;
+		}
 
-		//todo choose how to assign number of 1024 sons
-
+		//scorro percorso
+		if (i==1){
+			//todo choose how to assign number of 1024 sons
+			//root->sons[1024]
+			printf("--> %s\n", tokens[0]);
+			index =hashFunction(tokens[0], 0);
+			root->sons[index]=initNode(root->sons, tokens[i]);
+			root->numberOfSons= root->numberOfSons+1;
+		
+		}
+	
 	}
+	return root;
 }
 
 node* initTree(node * root){
-	if(root = (node*) malloc(sizeof(node)*1024)){
+	if(root = (node*) malloc(sizeof(node))){
 		root->name= "root";
 		root->numberOfSons= 0;
 		root->type= 1;
@@ -71,27 +102,74 @@ node* initTree(node * root){
 return root;
 }
 
+node* initNode(node * n, char * name){
+	if(n = (node*) malloc(sizeof(node))){
+		n->name= name;
+		n->numberOfSons= 0;
+		n->type= 1;
+	}	
+
+	return n;
+}
+
 void scorriAlbero(node* root){
 	node* temp;
-
 	temp=root;
-	if(temp->numberOfSons == 0)
+	if(temp->numberOfSons == 0){
 		printf("%s \n",root->name);
+		printf("n: %d \n", root->numberOfSons);
+	}
+
 	else{
-		printf("Albero non vuoto");
+		printf("Albero non vuoto\n");
 	}
 
 }
 
-unsigned long hash(unsigned char *str)
-{
+unsigned long hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
     while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) ^c ; /*hash * 33 + c */
 
     return hash;
 }
+
+long hashFunction(unsigned char *str, int trial){
+	uint32_t index;
+	unsigned long h= hash(str);
+
+	index = h+(0.5*trial)+(0.5*(trial*trial)); //quadratic probing
+	printf("index: --> %ld\n", index);
+
+	index= floor(1024*((index*(0.5*(sqrt(5) - 1)))-floor((index*(0.5*(sqrt(5) - 1))))));
+
+	printf("--- index: %d\n",index);
+
+	return index;
+}
+
+void getTokens(char tokens[][maxLen+1] , char path[maxPath] ){
+   char s[2] = "/";
+   char *tkn;
+   int i=0;
+   
+   /* get the first token */
+   tkn = strtok(path, s);
+   
+   /* walk through other tokens */
+   while( tkn != NULL && i<=maxLen ) 
+   {
+      strcpy(tokens[i], tkn);
+      i++;
+      tkn = strtok(NULL, s);
+   }
+
+}
+
+
+
+
 
 
